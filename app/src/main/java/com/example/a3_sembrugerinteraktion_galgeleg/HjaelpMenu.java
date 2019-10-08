7094ca101f38f12b011466bbe910ae07941c0a6e
JayVeeDee <jayveedees@gmail.com>
2019-10-09 00:28:23 +0200

    Lavet custom grafik og mere backgrounds lol. Det her tager længere tid at lave end opgaven. Send kill

--- app/src/main/java/com/example/a3_sembrugerinteraktion_galgeleg/HjaelpMenu.java
+++ app/src/main/java/com/example/a3_sembrugerinteraktion_galgeleg/HjaelpMenu.java
@@ -3,12 +3,21 @@ package com.example.a3_sembrugerinteraktion_galgeleg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class HjaelpMenu extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hjaelp);
+
+        image = findViewById(R.id.imageView);
+        Animation aniFade = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fadein);
+        image.startAnimation(aniFade);
     }
 }
--- app/src/main/java/com/example/a3_sembrugerinteraktion_galgeleg/PlayMenu.java
+++ app/src/main/java/com/example/a3_sembrugerinteraktion_galgeleg/PlayMenu.java
@@ -2,13 +2,26 @@ package com.example.a3_sembrugerinteraktion_galgeleg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class PlayMenu extends AppCompatActivity {
+
+    ImageView iStats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_menu);
+
+        iStats = findViewById(R.id.iStats); iStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PlayMenu.this, StatisticsMenu.class));
+            }
+        });
     }
 }
